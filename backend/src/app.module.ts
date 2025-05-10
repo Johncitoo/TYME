b9fcc80df5aa4b2d6be183cebd3c1f6f043e3242
Johncitoo <juan.contreras03@alumnos.ucn.cl>
2025-05-10 17:59:01 -0400

    si

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -15,7 +15,7 @@ import { AppService } from './app.service';
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT ?? '5432', 10),
       username: process.env.DB_USER,
-      password: process.env.DB_PASS,
+      password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       autoLoadEntities: true, // Busca automáticamente entidades con @Entity
       synchronize: true, // Solo en desarrollo. NO en producción
